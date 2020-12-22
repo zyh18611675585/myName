219e839197df349a88d214d8b25af705b608660b
Your Name <you@example.com>
2020-12-22 20:13:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      App组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // lintOnSave:false,//关闭ESLINT的规则检查
+  lintOnSave: "warning",//输出提示错误,但是项目继续运行
+};
